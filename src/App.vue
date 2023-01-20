fda64c01cd0c6831a9407e577bc1b35f8939ad12
GuglielmoBini <guglielmobini@gmail.com>
2023-01-20 18:33:54 +0100

    created header

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,25 @@ body {
   font-family: "Open Sans", sans-serif;
   min-height: 100vh;
 }
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+li {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+  font-weight: bold;
+  padding: 20px 10px;
+}
+
+/* utils */
+.container {
+  max-width: 1140px;
+  margin: 0 auto;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,6 +3,7 @@ export default {
     name: "AppHeader",
     data() {
         return {
+            // array oggetti navbar
             links: [
                 {
                     text: 'Characters',
@@ -60,8 +61,47 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <header class="container">
+        <nav class="navbar">
+            <figure>
+                <img src="../assets/img/dc-logo.png" alt="dc_logo">
+            </figure>
+            <ul>
+                <li v-for="link in links" :key="link.text"><a :href="link.url" :class="{ 'active': link.current }">{{
+                    link.text
+                }}</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
 
 <style lang="scss" scoped>
+header {
+    margin-top: 20px;
+    min-height: 100px;
 
+    .navbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+    }
+
+    figure {
+        width: 85px;
+    }
+
+    ul {
+        display: flex;
+    }
+
+    a {
+        color: black;
+    }
+
+    a.active {
+        color: #0182F9;
+    }
+}
 </style>
