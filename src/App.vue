6a699ab3f1bb47a2f22e438843606dd682d1b225
GuglielmoBini <guglielmobini@gmail.com>
2023-01-20 17:03:02 +0100

    created apps and components, linked components to app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  name: "App",
+  components: { AppHeader, AppMain, AppFooter }
+}
+</script>
+
+<template>
+  <app-header></app-header>
+  <app-main></app-main>
+  <app-footer></app-footer>
+</template>
+
+<style>
+/* reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* generics */
+body {
+  font-family: "Open Sans", sans-serif;
+  min-height: 100vh;
+}
+</style>
