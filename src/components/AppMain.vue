9dae14850064ee1255e52b53da23f85a40f4523f
GuglielmoBini <guglielmobini@gmail.com>
2023-01-23 17:30:48 +0100

    added product cards component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,25 @@
 <script>
+import ProductCard from './products/ProductCard.vue';
 export default {
-    name: "AppMain"
-}
+    name: "AppMain",
+    components: { ProductCard },
+    props: {
+        products: Array
+    }
+};
 </script>
 
 <template>
     <main>
         <!-- main-content -->
-        <section id="main-content">
-            <h2>Content goes here</h2>
+        <section id="products">
+            <div class="container">
+                <h3>Current Series</h3>
+                <div class="card-container">
+                    <product-card v-for="product in products" :key="product.series" :product="product"></product-card>
+                    <a href="#">Load More</a>
+                </div>
+            </div>
         </section>
     </main>
 </template>
@@ -18,10 +29,31 @@ export default {
 @use '../assets/scss/partials/mixins' as *;
 @use '../assets/scss/partials/variables' as *;
 
-#main-content {
-    @include flex-center;
+#products {
+    padding: 30px 0;
     min-height: 300px;
     color: white;
     background-color: $almostblack;
+
+    h3 {
+        @include main-buttons;
+        display: inline-block;
+    }
+
+    .card-container {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin-top: 20px;
+
+        .product-card {
+            flex-basis: calc(100% / 6);
+        }
+
+        a {
+            @include main-buttons;
+            color: white;
+        }
+    }
 }
 </style>
---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "ProductCard",
+    props: {
+        product: Object
+    }
+}
+</script>
+
+<template>
+    <div class="product-card">
+        <figure>
+            <a href="#"><img :src="product.thumb" :alt="product.series"></a>
+        </figure>
+        <figcaption>{{ product.series }}</figcaption>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.product-card {
+    padding: 15px;
+
+    figure img {
+        display: block;
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+        object-position: top;
+    }
+
+    figcaption {
+        margin-top: 15px;
+        text-transform: uppercase;
+    }
+}
+</style>
